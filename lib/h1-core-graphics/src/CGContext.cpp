74913f85316be6bff52b2c8815d497cbc17d2e50
亨睿 <henry.lx@alibaba-inc.com>
2019-09-11 14:14:37 +0800

    Add CGContext

---
+++ lib/h1-core-graphics/src/CGContext.cpp
@@ -0,0 +1,4 @@
+#include "CGContext.h"
+
+CGContext::CGContext() {
+}
---
+++ lib/h1-core-graphics/src/CGContext.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <Arduino.h>
+
+// A class represents XXX.
+class CGContext {
+public:
+  // Creates a new instance of `CGContext`.
+  CGContext();
+
+private:
+};
