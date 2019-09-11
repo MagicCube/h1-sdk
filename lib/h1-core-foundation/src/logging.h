4d04293c1a79125dbca166cecf674ebb7fc96ecc
亨睿 <henry.lx@alibaba-inc.com>
2019-09-11 10:47:43 +0800

    Add comments

--- lib/h1-core-foundation/src/logging.h
+++ lib/h1-core-foundation/src/logging.h
@@ -6,16 +6,17 @@
 
 using namespace std;
 
-inline void LOG_I(const string fmt_str, ...) {
+// Output a debug log to serial with [formatString](https://alvinalexander.com/programming/printf-format-cheat-sheet).
+inline void LOG_I(const string formatString, ...) {
   Serial.print("[INF] ");
-  int final_n, n = ((int)fmt_str.size()) * 2; /* Reserve two times as much as the length of the fmt_str */
+  int final_n, n = ((int)formatString.size()) * 2; /* Reserve two times as much as the length of the formatString */
   unique_ptr<char[]> formatted;
   va_list ap;
   while (1) {
     formatted.reset(new char[n]); /* Wrap the plain char array into the unique_ptr */
-    strcpy(&formatted[0], fmt_str.c_str());
-    va_start(ap, fmt_str);
-    final_n = vsnprintf(&formatted[0], n, fmt_str.c_str(), ap);
+    strcpy(&formatted[0], formatString.c_str());
+    va_start(ap, formatString);
+    final_n = vsnprintf(&formatted[0], n, formatString.c_str(), ap);
     va_end(ap);
     if (final_n < 0 || final_n >= n) {
       n += abs(final_n - n + 1);
@@ -26,17 +27,18 @@ inline void LOG_I(const string fmt_str, ...) {
   Serial.println(formatted.get());
 }
 
-inline void LOG_D(const string fmt_str, ...) {
+// Output a debug log to serial with [formatString](https://alvinalexander.com/programming/printf-format-cheat-sheet).
+inline void LOG_D(const string formatString, ...) {
 #ifdef DEBUG
   Serial.print("[DBG] ");
-  int final_n, n = ((int)fmt_str.size()) * 2; /* Reserve two times as much as the length of the fmt_str */
+  int final_n, n = ((int)formatString.size()) * 2; /* Reserve two times as much as the length of the formatString */
   unique_ptr<char[]> formatted;
   va_list ap;
   while (1) {
     formatted.reset(new char[n]); /* Wrap the plain char array into the unique_ptr */
-    strcpy(&formatted[0], fmt_str.c_str());
-    va_start(ap, fmt_str);
-    final_n = vsnprintf(&formatted[0], n, fmt_str.c_str(), ap);
+    strcpy(&formatted[0], formatString.c_str());
+    va_start(ap, formatString);
+    final_n = vsnprintf(&formatted[0], n, formatString.c_str(), ap);
     va_end(ap);
     if (final_n < 0 || final_n >= n) {
       n += abs(final_n - n + 1);
