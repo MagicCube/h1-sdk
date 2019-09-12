4c42421f9448c15197c4db0ff89abd376ad92ebb
亨睿 <henry.lx@alibaba-inc.com>
2019-09-12 15:20:28 +0800

    Add predefined colors

---
+++ lib/h1-core-graphics/src/colors.h
@@ -0,0 +1,19 @@
+#define COLOR_BLACK 0x0000       /*   0,   0,   0 */
+#define COLOR_NAVY 0x000F        /*   0,   0, 128 */
+#define COLOR_DARKGREEN 0x03E0   /*   0, 128,   0 */
+#define COLOR_DARKCYAN 0x03EF    /*   0, 128, 128 */
+#define COLOR_MAROON 0x7800      /* 128,   0,   0 */
+#define COLOR_PURPLE 0x780F      /* 128,   0, 128 */
+#define COLOR_OLIVE 0x7BE0       /* 128, 128,   0 */
+#define COLOR_LIGHTGREY 0xC618   /* 192, 192, 192 */
+#define COLOR_DARKGREY 0x7BEF    /* 128, 128, 128 */
+#define COLOR_BLUE 0x001F        /*   0,   0, 255 */
+#define COLOR_GREEN 0x07E0       /*   0, 255,   0 */
+#define COLOR_CYAN 0x07FF        /*   0, 255, 255 */
+#define COLOR_RED 0xF800         /* 255,   0,   0 */
+#define COLOR_MAGENTA 0xF81F     /* 255,   0, 255 */
+#define COLOR_YELLOW 0xFFE0      /* 255, 255,   0 */
+#define COLOR_WHITE 0xFFFF       /* 255, 255, 255 */
+#define COLOR_ORANGE 0xFDA0      /* 255, 180,   0 */
+#define COLOR_GREENYELLOW 0xB7E0 /* 180, 255,   0 */
+#define COLOR_PINK 0xFC9F
