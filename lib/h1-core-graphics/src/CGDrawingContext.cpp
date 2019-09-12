22b300ff31596e211fab663dc74a5582b9586b21
亨睿 <henry.lx@alibaba-inc.com>
2019-09-12 13:59:57 +0800

    Add TFT_eSPI

--- lib/h1-core-graphics/src/CGDrawingContext.cpp
+++ lib/h1-core-graphics/src/CGDrawingContext.cpp
@@ -1,4 +1,73 @@
 #include "CGDrawingContext.h"
 
-CGDrawingContext::CGDrawingContext() {
+CGDrawingContext::CGDrawingContext(CGRect frame) {
+  _frame = frame;
+  _zeroTranslation = _frame.equals(CGRectZero);
+}
+
+CGDrawingContext::CGDrawingContext(CGRect frame, TFT_eSPI *canvas) : CGDrawingContext(frame) {
+  _canvas = canvas;
+}
+
+CGRect CGDrawingContext::frame() {
+  return _frame;
+}
+
+void CGDrawingContext::drawPixel(CGPoint point, cg_color_t color) {
+  auto absPoint = translatePoint(point);
+  _canvas->drawPixel(absPoint.x, absPoint.y, color);
+}
+
+void CGDrawingContext::drawLine(CGPoint p1, CGPoint p2, cg_color_t color) {
+  auto absP1 = translatePoint(p1);
+  auto absP2 = translatePoint(p2);
+  _canvas->drawLine(absP1.x, absP1.y, absP2.x, absP2.y, color);
+}
+
+void CGDrawingContext::drawFastVLine(CGPoint origin, cg_unit_t height, cg_color_t color) {
+  auto absOrigin = translatePoint(origin);
+  _canvas->drawFastVLine(absOrigin.x, absOrigin.y, height, color);
+}
+
+void CGDrawingContext::drawFastHLine(CGPoint origin, cg_unit_t width, cg_color_t color) {
+  auto absOrigin = translatePoint(origin);
+  _canvas->drawFastHLine(absOrigin.x, absOrigin.y, width, color);
+}
+
+void CGDrawingContext::drawRect(CGRect rect, cg_color_t color) {
+  auto absRect = translateRect(rect);
+  _canvas->drawRect(absRect.origin.x, absRect.origin.y, absRect.size.width, absRect.size.height, color);
+}
+
+void CGDrawingContext::drawRoundRect(CGRect rect, cg_unit_t roundness, cg_color_t color) {
+  auto absRect = translateRect(rect);
+  _canvas->drawRoundRect(absRect.origin.x, absRect.origin.y, absRect.size.width, absRect.size.height, roundness, color);
+}
+
+void CGDrawingContext::drawCircle(CGPoint center, cg_unit_t radius, cg_color_t color) {
+  auto absCenter = translatePoint(center);
+  _canvas->drawCircle(absCenter.x, absCenter.y, radius, color);
+}
+
+void CGDrawingContext::drawString(String string, CGPoint position) {
+  auto absPosition = translatePoint(position);
+  _canvas->drawString(string, absPosition.x, absPosition.y);
+}
+
+void CGDrawingContext::fill(cg_color_t color) {
+}
+
+void CGDrawingContext::fillRect(CGRect rect, cg_color_t color) {
+  auto absRect = translateRect(rect);
+  _canvas->fillRect(absRect.origin.x, absRect.origin.y, absRect.size.width, absRect.size.height, color);
+}
+
+void CGDrawingContext::fillRoundRect(CGRect rect, cg_unit_t roundness, cg_color_t color) {
+  auto absRect = translateRect(rect);
+  _canvas->fillRoundRect(absRect.origin.x, absRect.origin.y, absRect.size.width, absRect.size.height, roundness, color);
+}
+
+void CGDrawingContext::fillCircle(CGPoint center, cg_unit_t radius, cg_color_t color) {
+  auto absCenter = translatePoint(center);
+  _canvas->fillCircle(absCenter.x, absCenter.y, radius, color);
 }
--- lib/h1-core-graphics/src/CGDrawingContext.h
+++ lib/h1-core-graphics/src/CGDrawingContext.h
@@ -2,11 +2,98 @@
 
 #include <Arduino.h>
 
-// A class represents XXX.
+#include <TFT_eSPI.h>
+
+#include "CGPoint.h"
+#include "CGRect.h"
+#include "CGSize.h"
+#include "types.h"
+
+// A class describes visual content using draw, push, and pop commands.
+// A drawing context can be created from screen, views or a bitmap.
+//
+// NOTE: All the drawing and filling commands are in relative coordinate.
 class CGDrawingContext {
 public:
   // Creates a new instance of `CGDrawingContext`.
-  CGDrawingContext();
+  CGDrawingContext(CGRect frame);
+
+  // Creates a new instance of `CGDrawingContext`.
+  CGDrawingContext(CGRect frame, TFT_eSPI *tft);
+
+  // Gets a `CGRect` represents origin and size of the contextual canvas in
+  // absolute coordinate.
+  CGRect frame();
+
+  // Draws a pixel at given point.
+  void drawPixel(CGPoint point, cg_color_t color);
+
+  // Draws a line connecting 2 given points.
+  void drawLine(CGPoint p1, CGPoint p2, cg_color_t color);
+
+  // Draws a vertical line from origin with given length.
+  void drawFastVLine(CGPoint origin, cg_unit_t height, cg_color_t color);
+
+  // Draws a horizontal line from origin with given length.
+  void drawFastHLine(CGPoint origin, cg_unit_t width, cg_color_t color);
+
+  // Draws the specific rectangle.
+  void drawRect(CGRect rect, cg_color_t color);
+
+  // Draws the specific round rectangle.
+  void drawRoundRect(CGRect rect, cg_unit_t roundness, cg_color_t color);
+
+  // Draws the specific circle.
+  void drawCircle(CGPoint center, cg_unit_t radius, cg_color_t color);
+
+  // Draws text at given position.
+  // You can specify font and color by font(v), fontSize(v) and
+  // textColor(v), while use textAlign(v) to set text alignment.
+  void drawString(String string, CGPoint position);
+
+  // Fills the contextual canvas with given color.
+  void fill(cg_color_t color);
+
+  // Fills the specific rectangle;
+  void fillRect(CGRect rect, cg_color_t color);
+
+  // Fills the specific round rectangle;
+  void fillRoundRect(CGRect rect, cg_unit_t roundness, cg_color_t color);
+
+  // Fills the specific circle;
+  void fillCircle(CGPoint center, cg_unit_t radius, cg_color_t color);
+
+  // Translates a local x-coordinate value to absolute.
+  cg_unit_t translateX(cg_unit_t relativeX) {
+    return _frame.origin.x + relativeX;
+  }
+
+  // Translates a local y-coordinate value to absolute.
+  cg_unit_t translateY(cg_unit_t relativeY) {
+    return _frame.origin.y + relativeY;
+  }
+
+  // Translates a local coordinate point to absolute.
+  CGPoint translatePoint(CGPoint relativePoint) {
+    if (_zeroTranslation) {
+      return relativePoint;
+    }
+    return CGPoint(translateX(_frame.origin.x), translateY(_frame.origin.y));
+  }
+
+  // Translates a local coorinate rectangle to absolute.
+  CGRect translateRect(CGRect relativeRect) {
+    if (_zeroTranslation) {
+      return relativeRect;
+    }
+    return CGRect(translatePoint(relativeRect.origin), relativeRect.size);
+  }
 
 private:
+  CGRect _frame;
+
+  TFT_eSPI *_canvas;
+
+  // Equals true if the origin of this context is (0, 0).
+  bool _zeroTranslation;
 };
