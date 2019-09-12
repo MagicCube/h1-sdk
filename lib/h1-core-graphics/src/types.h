0a0e3604dad45fdc8b1c173f986ce46e1cca29aa
亨睿 <henry.lx@alibaba-inc.com>
2019-09-12 15:03:59 +0800

    Add CGColorDepth

--- lib/h1-core-graphics/src/types.h
+++ lib/h1-core-graphics/src/types.h
@@ -2,6 +2,18 @@
 
 #include <Arduino.h>
 
+// Specify the depth of color, also known as BPP (Bit per Pixel).
+enum class CGColorDepth {
+  // 1-bit color which contains 2 colors.
+  MONO_1_BIT = 1,
+
+  // 8-bit color which contains 256 colors.
+  COLOR_8_BIT = 8,
+
+  // 16-bit color which contains 65,536 colors.
+  COLOR_16_BIT = 16
+};
+
 // Define the unit type of 2D system.
 // Normally it's `int16_t`.
 typedef int16_t cg_unit_t;
