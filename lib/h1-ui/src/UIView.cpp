92f9491b334c18a33f598d120114d87af2c030c6
Henry Li <henry1943@163.com>
2019-09-16 22:18:09 +0800

    Add UIView

---
+++ lib/h1-ui/src/UIView.cpp
@@ -0,0 +1,40 @@
+#include "UIView.h"
+
+UIView::UIView(CGRect frame) {
+  _frame = frame;
+  _bounds = CGRect(CGPointZero, frame.size);
+}
+
+CGRect UIView::frame() {
+  return _frame;
+}
+
+CGRect UIView::bounds() {
+  return _bounds;
+}
+
+CGSize UIView::size() {
+  return _frame.size;
+}
+
+CGPoint UIView::origin() {
+  return _frame.origin;
+}
+
+bool UIView::needsRedraw() {
+  return _needsRedraw;
+}
+
+void UIView::setNeedsRedraw() {
+  _needsRedraw = true;
+}
+
+void UIView::redraw(bool force = false) {
+  if (force || needsRedraw()) {
+    draw();
+  }
+  _needsRedraw = false;
+}
+
+void UIView::draw() {
+}
---
+++ lib/h1-ui/src/UIView.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include <h1.CoreGraphics.h>
+
+// A class represents a view which is a rectangle area on the screen.
+class UIView {
+public:
+  // Creates a new instance of `UIView`.
+  UIView(CGRect frame);
+
+  // Gets a `CGRect` represents origin and size of the view in
+  // parental coordinate.
+  CGRect frame();
+
+  // Gets a `CGRect` represents origin and size of the view in
+  // relative coordinate which location is always (0, 0).
+  CGRect bounds();
+
+  // Gets the size of the view.
+  CGSize size();
+
+  // Gets the origin of the view in parental coordinate.
+  CGPoint origin();
+
+  // Gets a boolean value, true if the view needs redraw.
+  // The only way to set it true is to invoke `needsRedraw()`.
+  bool needsRedraw();
+
+  // Sets `needsRedraw()` to true.
+  void setNeedsRedraw();
+
+  // Try to redraw the image of view.
+  // It will only invoke `draw()` if the `force` parameter is `true` or `needsRedraw()` returns `true`.
+  void redraw(bool force = false);
+
+  // Draws the image of the view. The default implementation does nothing.
+  virtual void draw();
+
+private:
+  CGRect _frame;
+  CGRect _bounds;
+  bool _needsRedraw;
+};
